1b326487648be63b347daf8ef4efa0c820678c14
Luan Ogawa <ogawa.luan@gmail.com>
2020-07-21 20:54:16 -0300

    Dia 02/04 do curso

---
+++ js/components/Title/index.js
@@ -0,0 +1,3 @@
+function Title(css, textContent) {
+    return `<h1 style="${css[0]}${css[1]}">${textContent}</h1>`
+}
--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 const root = document.querySelector("#root")
 
-function newElement(tag, content){
-    const title = `<${tag}>${content}</${tag}>`
-    root.insertAdjacentHTML("beforeend", title)
+//function newElement(tag, content){
+//    const title = `<${tag}>${content}</${tag}>`
+//    root.insertAdjacentHTML("beforeend", title)
 
-}
+//}
 
 
-newElement("h1", "Difficulty")
+//newElement("h1", "Difficulty")
+const textTitle = 'Difficulty'
+
+const title = Title`
+    color: red;
+    font-size: 30px;
+    ${textTitle}
+`
+
+root.insertAdjacentHTML('beforeend', title)
